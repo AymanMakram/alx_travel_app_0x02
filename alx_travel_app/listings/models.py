a1da98b43d041e20387e152ab4d1a790bff6a0d4
Ayman Makram <93218346+AymanMakram@users.noreply.github.com>
2025-12-14 10:37:39 +0200

    Update models.py

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,4 +1,6 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class Listing(models.Model):
     title = models.CharField(max_length=255)
@@ -30,3 +32,19 @@ class Review(models.Model):
 
     def __str__(self):
         return f"Review {self.rating}/5"
+
+class Payment(models.Model):
+    STATUS_CHOICES = (
+        ("Pending", "Pending"),
+        ("Completed", "Completed"),
+        ("Failed", "Failed"),
+    )
+
+    booking_reference = models.CharField(max_length=100, unique=True)
+    transaction_id = models.CharField(max_length=200, blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.booking_reference} - {self.status}"
